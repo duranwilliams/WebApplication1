cccdc927347e3e21c7b7c320edf55de37329c4c6
Dj Williams <duran.evan.williams@gmail.com>
2025-10-30 18:52:24 -0400

    History: 2025.10.30 - Begin import file, continue to test changes, move file import out of Program.cs, db work to create a general schema for all files and metadata - Add implementation of OpenXml to read/modify .xlsx files, test and debug

--- WebApplication1/DatafileReader.cs
+++ WebApplication1/DatafileReader.cs
@@ -1,6 +1,10 @@
 ﻿// files: https://catalog.data.gov/dataset?q=&sort=metadata_created+desc
 // This thing will read the files and store what is necessary.
 
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+
+
 // This is just round 1 of planning this whole thing
 internal class DatafileReader
 {
@@ -10,6 +14,40 @@ internal class DatafileReader
         File.ReadLines(pth); // todo: read all lines and store what is needed for each file
         // Possible have 1 config per file to identify which fields to import
 
+        ReadExcelFile(pth);
         return File.ReadAllText(pth);
     }
+
+
+public static void ReadExcelFile(string filePath)
+{
+    using (SpreadsheetDocument document = SpreadsheetDocument.Open(filePath, false))
+    {
+        WorkbookPart workbookPart = document.WorkbookPart;
+        Sheets sheets = workbookPart.Workbook.Sheets;
+
+        foreach (Sheet sheet in sheets)
+        {
+            WorksheetPart worksheetPart = (WorksheetPart)workbookPart.GetPartById(sheet.Id);
+            SheetData sheetData = worksheetPart.Worksheet.GetFirstChild<SheetData>();
+
+            foreach (Row row in sheetData.Elements<Row>())
+            {
+                foreach (Cell cell in row.Elements<Cell>())
+                {
+                    string cellValue = GetCellValue(cell, workbookPart);
+                    Console.WriteLine(cellValue);
+                }
+            }
+        }
+    }
+}
+
+private static string GetCellValue(Cell cell, WorkbookPart workbookPart)
+{
+        // Logic to retrieve the cell value
+        // Handle shared strings and other data types
+        return String.Empty;
+}
+
 }
